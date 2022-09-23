3444de35372f2eac9bbe6c26d93e54599bba51eb
Williane Delfino <willy.pinheiro.94@gmail.com>
2022-09-23 13:01:55 -0300

    atualização na entidade estados

--- src/main/java/br/pbmulesoft/api/modelo/Estado.java
+++ src/main/java/br/pbmulesoft/api/modelo/Estado.java
@@ -1,34 +1,34 @@
 package br.pbmulesoft.api.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Estado {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
+	@OneToMany(mappedBy = "estados")
+	private List<Estado> nome = new ArrayList<>();
 	@ManyToMany
 	private String regiao;
 	private Integer populacao;
 	private String capital;
 	private Integer area;
 	
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
 	}
 	public String getRegiao() {
 		return regiao;
