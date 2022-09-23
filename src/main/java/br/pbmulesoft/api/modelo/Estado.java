d28c0a7037891e271ec509926eff3f34c0f26004
Williane Delfino <willy.pinheiro.94@gmail.com>
2022-09-23 12:58:02 -0300

    entidades principais criadas

--- src/main/java/br/pbmulesoft/api/modelo/Estado.java
+++ src/main/java/br/pbmulesoft/api/modelo/Estado.java
@@ -1,5 +1,58 @@
 package br.pbmulesoft.api.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Estado {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@ManyToMany
+	private String regiao;
+	private Integer populacao;
+	private String capital;
+	private Integer area;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRegiao() {
+		return regiao;
+	}
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+	public Integer getPopulacao() {
+		return populacao;
+	}
+	public void setPopulacao(Integer populacao) {
+		this.populacao = populacao;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public Integer getArea() {
+		return area;
+	}
+	public void setArea(Integer area) {
+		this.area = area;
+	}
+	
 }
